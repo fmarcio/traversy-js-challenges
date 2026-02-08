d29c6619b490f9cc8e46aee2efc7ffc9e41b5888
Marcio Fonseca <marcio.fonseca88@gmail.com>
2026-02-07 23:54:21 -0300

    Add solution for basic-challenges-1/03-calculator

--- 01-basic-challenges-1/03-calculator/calculator-run.js
+++ 01-basic-challenges-1/03-calculator/calculator-run.js
@@ -1,5 +1,12 @@
 const calculator = require('./calculator');
 
-const result = calculator(1, 2, '+');
+const sum = calculator(2, 2, '+');
+const sub = calculator(2, 3, '-');
+const multiply = calculator(3, 4, '*');
+const divide = calculator(10, 2, ';');
+const error = calculator(2, 2, '~');
 
-console.log(result);
+console.log(sum);
+console.log(sub);
+console.log(multiply);
+console.log(divide);

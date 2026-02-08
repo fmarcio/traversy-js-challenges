8a3eca5e40505e64d393ccf05d50e5de662491eb
Marcio Fonseca <marcio.fonseca88@gmail.com>
2026-02-08 20:43:34 -0300

    Create solution for 01-basic-challenges-1/04-count-occurrences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,24 @@
-function countOccurrences() {}
+// Using for loop
+function countOccurrencess(string, char) {
+  let count = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i].toLowerCase() === char.toLowerCase()) count++;
+  }
+
+  return count;
+}
+
+// Using string.split() method
+function countOccurrences(string, char) {
+  let splittedStrArr = string.split('');
+  let count = 0;
+
+  splittedStrArr.forEach((letter) => {
+    if (letter.toLowerCase() === char.toLowerCase()) count++;
+  });
+
+  return count;
+}
 
 module.exports = countOccurrences;
